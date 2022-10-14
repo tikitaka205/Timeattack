14a7d12690bd8decf9889959e9b67fd15fcba326
minsu <splendent77@gmail.com>
2022-10-14 20:14:59 +0900

    test

---
+++ django/user/urls.py
@@ -0,0 +1,13 @@
+from django.urls import path
+from . import views
+
+# from django.contrib.auth import views as auth_views
+
+
+urlpatterns = [
+    path('signup/', views.signup, name='signup'),
+    path('login/', views.login, name='login'),
+    path('home/', views.home, name='home'),
+    path('logout/', views.logout, name='logout'),
+
+]
